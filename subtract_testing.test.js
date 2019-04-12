c1d91b0beebcacfcb99b161df63db8ce0e5288be
eofafrica4lyf <aboderinemmanuel@gmail.com>
2019-04-12 05:46:41 -0700

    added test for rounded floating numbers

--- subtract_testing.test.js
+++ subtract_testing.test.js
@@ -6,4 +6,12 @@ test('Empty parameters; one parameter only provided',()=>{
 
 test('10 - 1 to equal 9',()=>{
     expect(sub(10,1)).toBe(9);
-})
+});
+
+test('Rounding floats; case of (0.2)-(-0.1)', ()=>{
+    expect(sub(0.2, -0.1)).toBeCloseTo(0.3)
+});
+test('Rounding floats; case of (0.1)-(-0.2)', ()=>{
+    expect(sub(0.1, -0.2)).toBeCloseTo(0.3)
+});
+
