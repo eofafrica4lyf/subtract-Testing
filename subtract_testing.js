9c66bc162386e8c44765bb0d8cd5a112def9de02
eofafrica4lyf <aboderinemmanuel@gmail.com>
2019-04-12 05:35:59 -0700

    added test for normal parameters

--- subtract_testing.js
+++ subtract_testing.js
@@ -1,5 +1,7 @@
 function subtract(firstNumber, secondNumber){
-    
+    if(firstNumber !== undefined || secondNumber !== undefined){
+        return firstNumber - secondNumber;
+    }
     return 'jdsfsfdsdf';
 }
 
--- subtract_testing.test.js
+++ subtract_testing.test.js
@@ -1,5 +1,9 @@
 const sub = require('./subtract_testing');
 
-test('Empty parameters',()=>{
+test('Empty parameters; one parameter only provided',()=>{
     expect(sub()).toBe('jdsfsfdsdf');
+});
+
+test('10 - 1 to equal 9',()=>{
+    expect(sub(10,1)).toBe(9);
 })
